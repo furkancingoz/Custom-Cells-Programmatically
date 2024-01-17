337876607aacb04ae7ec7be4afc22534c6fddfe6
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-01-17 20:59:35 +0300

    starting

--- Custom Cells Programmatically/ViewController.swift
+++ Custom Cells Programmatically/ViewController.swift
@@ -4,16 +4,96 @@
 //
 //  Created by Furkan Cingöz on 17.01.2024.
 //
-
+import SwiftUI
 import UIKit
 
 class ViewController: UIViewController {
 
+
+  //MARK: - Variables
+  private let image : [UIImage] = [
+    UIImage(named: "1")!,
+    UIImage(named: "2")!,
+    UIImage(named: "3")!,
+    UIImage(named: "4")!,
+    UIImage(named: "5")!,
+    UIImage(named: "6")!,
+    UIImage(named: "7")!,
+    UIImage(named: "8")!,
+    UIImage(named: "9")!,
+    UIImage(named: "10")!,
+    UIImage(named: "11")!,
+    UIImage(named: "12")!,
+    UIImage(named: "13")!,
+    UIImage(named: "14")!,
+    UIImage(named: "15")!,
+    ]
+
+
+  //MARK: - UI Components
+
+  private let tableView : UITableView = {
+    let tableView = UITableView()
+    tableView.backgroundColor = .systemBackground
+    tableView.allowsSelection = true
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+
+    return tableView
+
+  }()
+
+
+  //MARK: - Lifecyle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    setupUI()
+      self.tableView.delegate = self
+      self.tableView.dataSource = self
+  }
+
+
+  //MARK: - Setup UI
+  private func setupUI() {
+    self.view.backgroundColor = .systemBlue
+    self.view.addSubview(tableView)
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+      tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+      tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+
+    ])
   }
 
 
 }
 
+#Preview {
+  ViewControllerViewRepresentable()
+}
+struct ViewControllerViewRepresentable: UIViewControllerRepresentable {
+  func makeUIViewController(context: Context) -> ViewController {
+    // PlayerDetailVC'nin örneğini oluştur.
+    return ViewController()
+  }
+
+  func updateUIViewController(_ uiViewController: ViewController, context: Context) {
+    // Bu fonksiyon, view controller'ı güncellemek için kullanılır.
+  }
+}
+
+
+extension ViewController : UITableViewDelegate , UITableViewDataSource {
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    <#code#>
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    <#code#>
+  }
+  
+
+}
