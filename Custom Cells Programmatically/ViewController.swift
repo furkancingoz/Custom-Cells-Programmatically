50352187feab1cfb609ee9c998f77b5bcda4da74
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-01-17 20:27:56 +0300

    Initial Commit

---
+++ Custom Cells Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Cells Programmatically
+//
+//  Created by Furkan Cingöz on 17.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
